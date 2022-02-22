80ab81f3d1f9657b393926b3c6258534bf7b35ef
HyeminJo <hyemin.jo1@cj.net>
2022-02-22 16:36:01 +0900

    update weatherJS

--- js/weather.js
+++ js/weather.js
@@ -1,61 +1,28 @@
-const weather = document.querySelector(".js-weather");
+//using https://openweathermap.org/current
 
 const API_KEY = "76f04d458c59bb274d5efe454183d524";
-const COORDS = "coords";
 
-function getWeather(lat, lon) {
-    // 가져올 데이터 url에서 얻기
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=kr&units=metric`
-    )
-        .then(function(response) {
-            // fetch 완료 후 then 실행
-            console.log(response);
-            return response.json();
-        })
-        .then(function(json) {
-            console.log(json);
-            const temperature = json.main.temp;
-            const place = json.name;
-            weather.innerText = `${temperature}℃ @ ${place}`;
+function onGeoSuc(position) {
+    // console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    // console.log(`${lat},${lon}`);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=kr&units=metric`;
+    // console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const weather = document.querySelector(".js-weather");
+            console.log(data);
+            const desc = data.weather[0].description;
+            const place = data.name;
+            weather.innerText=`${place}\n${desc}`;
         });
 }
 
-function saveCoords(coordsObj) {
-    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+function onGeoErr(error) {
+    alert("ERROR");
 }
 
-function handleGeoSuccess(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const coordsObj = {
-        latitude,
-        longitude
-    };
-    saveCoords(coordsObj);
-    getWeather(latitude, longitude);
-}
-
-function handleGeoError() {
-    console.log('Cant access geo location');
-}
-
-function askForCoords() {
-    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError)
-}
-
-function loadCoords() {
-    const loadedCoords = localStorage.getItem(COORDS);
-    if(loadedCoords === null) {
-        askForCoords();
-    } else {
-        //getWeather
-        const parseCoords = JSON.parse(loadedCoords);
-        getWeather(parseCoords.latitude, parseCoords.longitude);
-    }
-}
-
-function init() {
-    loadCoords();
-}
-
-init();
+navigator.geolocation.getCurrentPosition(onGeoSuc, onGeoErr);
